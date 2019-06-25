2afa8d346452983c01a36b87016991360d404fd7
MarionLegrandLogrami <tableau-salt-loire@logrami.fr>
2019-06-25 09:42:53 +0200

    correction to improve the way to specify the folder where the psql.exe program is installed

--- Installation/install_stacomi.cmd
+++ Installation/install_stacomi.cmd
@@ -24,9 +24,7 @@ set prog_psql=psql
 :: ------------- Do not change anything above this line -------------
 set prog_unzip=%~dp0%bin\unzip.exe
 set sql_data_zip=%~dp0%data\install_bd_contmig_nat.zip
-set sql_rep_unzip=%~dp0%data\
-set sql_data_unzip=%sql_rep_unzip%install_bd_contmig_nat.sql
-
+set sql_data_unzip=%~dp0%data\install_bd_contmig_nat.sql
 set db_name=bd_contmig_nat
 set odbc_driver_name_64=PostgreSQL Unicode(x64)
 set odbc_driver_name_32=PostgreSQL Unicode
@@ -37,10 +35,10 @@ set ret=0
 :: -----------------
 :GetOpts
 if "%1" == "-h" goto Usage
-if "%1" == "-u" set sql_user=%2& shift
-if "%1" == "-p" set sql_pass=%2& shift
-if "%1" == "-P" set sql_port=%2& shift
-if "%1" == "-H" set sql_host=%2& shift
+if "%1" == "-u" set sql_user=%2 & shift
+if "%1" == "-p" set sql_pass=%2 & shift
+if "%1" == "-P" set sql_port=%2 & shift
+if "%1" == "-H" set sql_host=%2 & shift
 shift
 if not "%1" == "" goto GetOpts
 goto CheckDependencies
@@ -71,15 +69,17 @@ where /Q %prog_psql%
 if "%ERRORLEVEL%" neq "0" (
 	echo   * Oops: psql.exe not found in your PATH environment variable... we will need it.
 	echo     Please give the full path of the executable.
-	echo      --^> It should be in a folder like: C:\Programs\PostgreSQL\^<version^>\bin
+	echo      --^> It should be in a folder like: "C:\Programs\PostgreSQL\^<version^>\bin\psql.exe"
 	set /P prog_psql=Full path? 
-	where /Q !prog_psql!
-	if "!ERRORLEVEL!" neq "0" (
+	if not exist !prog_psql! (
+REM	where /Q !prog_psql!
+REM	if "!ERRORLEVEL!" neq "0" (
 		echo This is not a valid path. Sorry.
 		set ret=1 && goto End
 	)
 )
 echo   * pSQL executable: OK
+
 :: Is there unzip?
 if not exist !prog_unzip! (
 	echo   * Oops: unzip.exe not found in 'bin' folder...
@@ -95,9 +95,8 @@ if not exist !sql_data_zip! (
 	set ret=1 && goto End
 )
 echo   * Database archive: OK
-
 :: Can we connect to PostgreSQL server?
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -c "SELECT 1" postgres >nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -c "SELECT 1" >nul
 if "%ERRORLEVEL%" neq "0" (
 	echo   * Oops: can't connect to PostgreSQL server. Please check your credentials,
 	echo        and be sure the server is running
@@ -119,13 +118,13 @@ echo   * Database connection: OK
 :Process
 echo Processing
 echo     * Extracting database archive
-%prog_unzip% -o -q %sql_data_zip% -d %sql_rep_unzip%
+%prog_unzip% -o -q %sql_data_zip% -d %~dp0%data
 if "%ERRORLEVEL%" neq "0" (
 	echo         * Error while extracting. Operation aborted
 	set ret=2 && goto End
 )
 echo     * Creating database '%db_name%'
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -c "CREATE DATABASE %db_name% WITH ENCODING = 'UTF8';" postgres >nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -c "CREATE DATABASE %db_name% WITH ENCODING = 'UTF8';" >nul
 if "%ERRORLEVEL%" neq "0" (
 	echo         * Error while creating database.
 	echo           If it already exists, you will have to drop it manually, and launch this script again later.
@@ -133,19 +132,19 @@ if "%ERRORLEVEL%" neq "0" (
 	set ret=2 && goto End
 )
 echo     * Creating connection role 'nat'
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -c "CREATE ROLE nat LOGIN PASSWORD 'nat' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" %db_name% >nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -c "CREATE ROLE nat LOGIN PASSWORD 'nat' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" >nul
 
 echo     * Creating connection role 'iav'
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -c "CREATE ROLE iav LOGIN PASSWORD 'iav' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" %db_name% >nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -c "CREATE ROLE iav LOGIN PASSWORD 'iav' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" >nul
 
 echo     * Creating connection role 'invite'
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -c "CREATE ROLE invite LOGIN PASSWORD 'invite' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" %db_name% >nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -c "CREATE ROLE invite LOGIN PASSWORD 'invite' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" >nul
 
 echo     * Creating connection role 'user_1'
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -c "CREATE ROLE user_1 LOGIN PASSWORD 'user_1' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" %db_name% >nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -c "CREATE ROLE user_1 LOGIN PASSWORD 'user_1' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;" >nul
 
 echo     * Filling database '%db_name%' ^(please be patient, it will take a while^)
-set PGPASSWORD=%sql_pass%&& !prog_psql! -U "%sql_user%" -h %sql_host% -p %sql_port% -f %sql_data_unzip% %db_name%>nul
+set PGPASSWORD=%sql_pass%&& !prog_psql! -U %sql_user% -h %sql_host% -p %sql_port% -f %sql_data_unzip% %db_name% >nul
 if "%ERRORLEVEL%" neq "0" (
 	echo         * Error while extracting. Operation aborted
 	set ret=2 && goto End
@@ -166,7 +165,7 @@ if exist "%ProgramFiles%\psqlODBC" (
 	echo         * Assuming we have a 32 bits driver
 	echo           ^(I did not find any "psqlODBC" folder in "%ProgramFiles%"^)
 )
-odbcconf /A {ConfigDSN "!odbc_driver!" "DSN=%db_name%|UID=%sql_user%|PWD=%sql_pass%|SERVER=%sql_host%|PORT=%sql_port%|DATABASE=%db_name%|DESCRIPTION=ODBC link created for stacomiR project"}
+odbcconf /A {ConfigDSN "!odbc_driver!" "DSN=%db_name%|UID=%sql_user%|PWD=%sql_pass%|SERVER=%sql_host%|PORT=%sql_port%|DATABASE=%db_name%|DESCRIPTION=popi"}
 
 
 :: Bye bye
